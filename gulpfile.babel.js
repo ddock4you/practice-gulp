18b5b24207c0dc0d9ef18f699f32c1f08db13eab
ddock4you <ddock4you@gmail.com>
2019-09-01 11:33:29 +0900

    gulp-webserver 설치 및 gulp 내 watch 기능 활성화

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,9 +3,11 @@ import gpug from "gulp-pug";
 // npm i gulp-pug -D
 import  del from "del";
 // npm i del
+import ws from 'gulp-webserver';
 
 const routes = {
     pug: {
+        watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
     }
@@ -19,14 +21,28 @@ const pug = () =>
 
 const clean = () => del(["build"]);
 
+const webserver = () => 
+    gulp.src("build").pipe(ws({
+        livereload: true,
+        open: true
+    }));
+// 중괄호를 삭제하고 실행하니 그제서야 제대로 실행됐음
+// es6에서 중괄호 있고 없고의 차이를 파악하기(return 말고 다른 기능이 있는지)
+
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+}
 
 // tesk 과정을 분류
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
+const openDev = gulp.series([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets]);
+
+
+export const dev = gulp.series([prepare, assets, openDev]);
 
 /*
     개선사항
