c3482de17176f09cc70ad7c38639f691f69e324e
ddock4you <ddock4you@gmail.com>
2019-09-01 12:21:42 +0900

    gulp-image 설치 및 실행

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,12 +4,20 @@ import gpug from "gulp-pug";
 import  del from "del";
 // npm i del
 import ws from 'gulp-webserver';
+import image from "gulp-image";
+// npm i gulp-image -D
+// gulp-image 설치 후 실행을 하면 취약점이 발견됐다면서 
+
 
 const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img: {
+        src: "src/img/*",
+        dest: "build/img"
     }
 }
 
@@ -31,10 +39,20 @@ const webserver = () =>
 
 const watch = () => {
     gulp.watch(routes.pug.watch, pug);
+    gulp.watch(routes.img.src, img);
 }
+// 파일 변동 있을 때 마다 pug와 img 함수를 실행
+
+
+const img = () => 
+    gulp
+        .src(routes.img.src)
+        .pipe(image())
+        .pipe(gulp.dest(routes.img.dest));
+
 
 // tesk 과정을 분류
-const prepare = gulp.series([clean]);
+const prepare = gulp.series([clean, img]);
 
 const assets = gulp.series([pug]);
 
